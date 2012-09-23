c370bead05c778569cefdcf95f9dd7ae3e4c543b
Dmytrii Nagirniak <dnagir@gmail.com>
2012-09-24 09:52:16 +1000

    Initial - new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sf666::Application.config.secret_token = 'c5092df53723eedd826b6e43a13e85a55d817d3419e06eab584be2839ceaddac62c5b59b6b11b6dcd6024803fd20bc706a2a41ef4e9dd8b65fe2a1cd75fcaed2'
